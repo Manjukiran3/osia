4baff830e9225ef57c742e6b71ef9afeb76c07e8
Daniel Khamsing <dkhamsing8@gmail.com>
2017-08-01 14:55:53 -0700

    add test

--- Xcode/OSIATests/OSIATests.m
+++ Xcode/OSIATests/OSIATests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Parser.h"
 
 @interface OSIATests : XCTestCase
 
@@ -14,26 +15,18 @@
 
 @implementation OSIATests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+- (void)testParse {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Parse"];
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+    Parser *p = [Parser new];
+    [p getLatestDataWithCompletion:^(Data *d, NSError *e) {
+        [expectation fulfill];
     }];
+    
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        if (error!=nil)
+            NSLog(@"Error: %@", error);
+    }];    
 }
 
 @end
